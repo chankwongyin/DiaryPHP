38257ccc408f6e77048059d74cba4eeee2f54eff
chankwongyin <chankwongyintony@gmail.com>
2019-05-30 17:59:13 +0800

    set up the base html skeleton

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dairy</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row" id="diaryPage"></div>
+        <div class="row" id="loginPage"></div>
+        <div class="row" id="logoutPage"></div>
+        <div class="row" id="signupPage"></div>
+    </div>
+</body>
+
+</html>
