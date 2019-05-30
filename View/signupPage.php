96ff99981017dd5b127cdd5ee333a490779916bc
chankwongyin <chankwongyintony@gmail.com>
2019-05-30 18:34:50 +0800

    add form to signupPage.php

---
+++ View/signupPage.php
@@ -0,0 +1,16 @@
+<form>
+    <div class="form-group">
+        <label for="email">Email address</label>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp"
+            placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+    </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" name="stayLoggedIn" id="stayLoggedIn">
+        <label class="form-check-label" for="stayLoggedIn">Keep Me Logged In</label>
+    </div>
+    <button type="submit" class="btn btn-primary" name="signup">Sign Up</button>
+</form>
--- index.php
+++ index.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dairy</title>
-    <link rel="stylesheet" href="<link rel=" stylesheet"
-        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    <link rel=" stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/index.css">
 </head>
@@ -15,15 +14,39 @@
 <body>
     <div class="container">
         <div class="row">
-            <header>
-                <h1>Diary</h1>
-                <h2>Created by CHAN Kwong Yin</h2>
-            </header>
+            <div class="col">
+                <header>
+                    <h1>Diary</h1>
+                    <h2>Created by CHAN Kwong Yin</h2>
+                </header>
+                <hr>
+            </div>
         </div>
-        <div class="row" id="diaryPage"></div>
-        <div class="row" id="loginPage"></div>
-        <div class="row" id="logoutPage"></div>
-        <div class="row" id="signupPage"></div>
+        <div class="row" id="diaryPage">
+            <div class="col">
+                <?php include "./View/diaryPage.php"?>
+            </div>
+            </div>
+        <div class="row" id="loginPage">
+            <div class="col">
+                <?php include "./View/loginPage.php"?>
+            </div>
+        </div>
+        <div class="row" id="logoutPage">
+            <div class="col">
+                <?php include "./View/logoutPage.php"?>
+            </div>
+        </div>
+        <div class="row" id="signupPage">
+            <div class="col">
+                <?php include "./View/signupPage.php"?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="button" class="btn btn-dark">Switch to</button>
+            </div>
+        </div>
     </div>
 </body>
 
