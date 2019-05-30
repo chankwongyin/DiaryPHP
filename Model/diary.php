15eb89017e0a024fc114406839349de89d37d0f0
chankwongyin <chankwongyintony@gmail.com>
2019-05-30 23:57:52 +0800

    add diary model

---
+++ Model/diary.php
@@ -0,0 +1,33 @@
+<?php
+
+class Diary
+{
+    private $title;
+    private $content;
+
+    public function getTitle()
+    {
+        return $title;
+    }
+
+    public function getContent()
+    {
+        return $content;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function __construct($title = "", $content = "")
+    {
+        $this->title = $title;
+        $this->content = $content;
+    }
+
+}
