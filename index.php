785f6e865c6f83c2957e12b95788109a62960621
chankwongyin <chankwongyintony@gmail.com>
2019-05-30 18:16:42 +0800

    add loginPage form

--- index.php
+++ index.php
@@ -6,10 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dairy</title>
+    <link rel="stylesheet" href="<link rel=" stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 
 <body>
     <div class="container">
+        <div class="row">
+            <header>
+                <h1>Diary</h1>
+                <h2>Created by CHAN Kwong Yin</h2>
+            </header>
+        </div>
         <div class="row" id="diaryPage"></div>
         <div class="row" id="loginPage"></div>
         <div class="row" id="logoutPage"></div>
