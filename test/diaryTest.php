72f01878db0e9ad9d9975466b5749ec07364479a
chankwongyin <chankwongyintony@gmail.com>
2019-05-31 10:38:46 +0800

    add diaryTest.php

---
+++ test/diaryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+include "../Model/diary.php"
+
+$title = "test title";
+$content ="test content";
+
+$diary = new Diary($title,$content);
+
+if($diary->getTitle() != $title){
+    echo "getTitle() error";
+}
+
+if($diary->getContent() != $content){
+    echo "getContent() error";
+}
+
+$newTitle = "new title";
+$newContent = "new content";
+
+$diary->setContent($newContent);
+$diary->setTitle($newTitle);
+
+if ($diary->getTitle() != $newTitle) {
+    echo "setTitle() error";
+}
+
+if ($diary->getContent() != $newContent) {
+    echo "setContent() error";
+}
+
+echo $diary->printDiary();
+
+?>
---
+++ test/userTest.php
@@ -0,0 +1,5 @@
+<?php
+
+include "../Model/user.php"
+
+?>
